0ced83baa66f2a925863342fe2baed0ba0edc88c
Jayesh <jayeshbnr@gmail.com>
2026-08-04 17:19:07 +0530

    Corrected the class name for database model.

--- helpers.py
+++ helpers.py
@@ -1,10 +1,10 @@
-from models import Users, db
+from models import User, db
 from flask import session
 
 def create_admin():
 
-    if not Users.query.filter_by(role = 'admin').first():
-        admin = Users(role = 'admin', user_name = 'trek-admin', status = 'Approved',
+    if not User.query.filter_by(role = 'admin').first():
+        admin = User(role = 'admin', user_name = 'trek-admin', status = 'Approved',
                       f_name = 'Administrator', isBlackListed = False)
         admin.set_password('123456')
         db.session.add(admin)
@@ -14,5 +14,5 @@ def logged_in():
 
     user_id = session.get('user_id')
     if user_id:
-        return Users.query.get_or_404(user_id)
+        return User.query.get_or_404(user_id)
     return None
